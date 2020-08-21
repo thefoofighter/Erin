7013e251cd913b79a4f7149d2ca9cb5cdab579d1
Cathal McNally <mcnallycathal@yahoo.ie>
2020-08-21 09:58:23 +0200

    makefiixes

--- Tools/BuildScripts/premake-gmake-mac.sh
+++ Tools/BuildScripts/premake-gmake-mac.sh
@@ -5,3 +5,6 @@ cd ../../
 ./Tools/Binaries/premake/premake5-mac --file=erin-premake-config.lua --os=macosx gmake
 rm erin-premake-config.lua
 rm Erin/3rdparty/glfw/glfw-premake-config.lua
+
+sed -i -e 's/--no-print-directory/--print-directory/g' Makefile
+rm Makefile-e
--- Tools/CleanScripts/clean-unix.sh
+++ Tools/CleanScripts/clean-unix.sh
@@ -8,5 +8,9 @@ find . -name "*.vcxproj" -type f -delete
 find . -name "*.vcxproj.filters" -type f -delete
 find . -name "*.vcxproj.user" -type f -delete
 
+rm -fr Erin.xcworkspace/
+rm -fr Sandbox/Sandbox.xcodeproj/
+rm -fr Erin/Erin.xcodeproj/
+rm -fr Erin/3rdparty/glfw/GLFW.xcodeproj/
 rm -fr Build/
 rm -fr Erin/3rdparty/glfw/Build
