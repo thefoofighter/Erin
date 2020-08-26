955b1ae8d89f54bba601d53f5a23f05f50e30f00
Cathal <mcnallycathal@yahoo.ie>
2020-08-26 23:28:57 +0200

    add own spdlog, add sed for windows, modify windows generator for gmake

--- Tools/BuildScripts/premake-gmake-win.bat
+++ Tools/BuildScripts/premake-gmake-win.bat
@@ -1,7 +1,15 @@
 xcopy config\glfw-premake-config.lua ..\..\Erin\3rdparty\glfw
+xcopy config\glad-premake-config.lua ..\..\Erin\3rdparty\glad
 xcopy config\erin-premake-config.lua ..\..\
+
 chdir ..\..\
+
 call Tools\Binaries\premake\premake5-win.exe --file=erin-premake-config.lua --os=windows gmake
+
 del erin-premake-config.lua
 del Erin\3rdparty\glfw\glfw-premake-config.lua
+del Erin\3rdparty\glad\glad-premake-config.lua
+
+call Tools\Binaries\sed\sed.exe -i -e "s/--no-print-directory/--print-directory/g" Makefile
+
 pause
